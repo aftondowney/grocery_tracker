661ec3afc8a9b0ab13b36529875c2519ca57738d
Afton Downey and Haley Bristol <aaike001@gmail.com>
2016-02-01 11:35:45 -0800

    add html

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
-function User(firstName, lastName, email) {
+function User(firstName, email) {
   this.firstName = firstName;
-  this.lastName = lastName;
   this.email = email;
   this.pantry = [];
   this.shopping = [];
 }
 
-function Pantry(itemName, itemQuantity, itemUsage, itemExpiration) {
+function Pantry(itemName, itemQuantity, itemUsage, itemExpiration, itemDate) {
   this.itemName = itemName;
   this.itemQuantity = itemQuantity;
   this.itemUsage = itemUsage;
   this.itemExpiration = itemExpiration;
+  this.itemDate = itemDate;
 }
 
 function Shopping(itemName, itemQuantity) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,7 @@
 describe('User', function() {
   it("creates a new user with given inputs", function() {
-    var testUser = new User ("Tina", "Turner", "turner@gmail.com");
-    expect(testUser.firstName).to.equal("Tina");
-    expect(testUser.lastName).to.equal("Turner");
+    var testUser = new User ("Tina", "turner@gmail.com");
+    expect(testUser.firstName).to.equal("Tina Turner");
     expect(testUser.email).to.equal("turner@gmail.com");
     expect(testUser.pantry).to.eql([]);
     expect(testUser.shopping).to.eql([]);
@@ -11,11 +10,12 @@ describe('User', function() {
 
 describe('Pantry', function() {
   it("creates a new list of items already existing in users pantry", function() {
-    var testPantry = new Pantry ("banana", 3, 1, "02/01/2016");
+    var testPantry = new Pantry ("banana", 3, 1, "02/11/2016", "02/01/2016");
     expect(testPantry.itemName).to.equal("banana");
     expect(testPantry.itemQuantity).to.equal(3);
     expect(testPantry.itemUsage).to.equal(1);
-    expect(testPantry.itemExpiration).to.equal("02/01/2016");
+    expect(testPantry.itemExpiration).to.equal("02/11/2016");
+    expect(testPantry.itemDate).to.equal("02/01/2016");
   });
 });
 
