ef32e1df25284ba750fb508305016e7b4a1ea7a0
Afton Downey and Haley Bristol <hebristol@gmail.com>
2016-02-01 14:14:53 -0800

    add html buttons and form

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,45 @@ function User(firstName, email) {
   this.shopping = [];
 }
 
-function Pantry(itemName, itemQuantity, itemUsage, itemExpiration, itemDate) {
+function Pantry(itemName, itemQuantity) {
   this.itemName = itemName;
   this.itemQuantity = itemQuantity;
-  this.itemUsage = itemUsage;
-  this.itemExpiration = itemExpiration;
-  this.itemDate = itemDate;
 }
 
 function Shopping(itemName, itemQuantity) {
   this.itemName = itemName;
   this.itemQuantity = itemQuantity;
 }
+
+function resetFields() {
+  $("input#new-item").val("");
+  $("textarea#new-quantity").val("");
+}
+
+
+$(document).ready(function() {
+  $("#shoppingList").click(function() {
+
+    $("#shoppingList").append('<div class="form-group">' +
+                       '<label for="new-item">Item Name</label>' +
+                       '<input type="text" class="form-control" id="new-item">' +
+                       '</div>' +
+                       '<div class="form-group">' +
+                       '<label for="new-quantity">Item Quantity</label>' +
+                       '<input type="text" class="form-control" id="new-quantity">' +
+                       '</div>' + '<button type="submit" class="btn" id="shoppingSubmit">Add</button>');
+  });
+
+ $("#pantryList").click(function() {
+
+   $("#pantryList").append('<div class="form-group">' +
+                      '<label for="new-item">Item Name</label>' +
+                      '<input type="text" class="form-control" id="new-item">' +
+                      '</div>' +
+                      '<div class="form-group">' +
+                      '<label for="new-quantity">Item Quantity</label>' +
+                      '<input type="text" class="form-control" id="new-quantity">' +
+                      '</div>' + '<button type="submit" class="btn" id="pantrySubmit">Add</button>');
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('User', function() {
   it("creates a new user with given inputs", function() {
-    var testUser = new User ("Tina", "turner@gmail.com");
+    var testUser = new User ("Tina Turner", "turner@gmail.com");
     expect(testUser.firstName).to.equal("Tina Turner");
     expect(testUser.email).to.equal("turner@gmail.com");
     expect(testUser.pantry).to.eql([]);
@@ -10,12 +10,10 @@ describe('User', function() {
 
 describe('Pantry', function() {
   it("creates a new list of items already existing in users pantry", function() {
-    var testPantry = new Pantry ("banana", 3, 1, "02/11/2016", "02/01/2016");
+    var testPantry = new Pantry ("banana", 3);
     expect(testPantry.itemName).to.equal("banana");
     expect(testPantry.itemQuantity).to.equal(3);
-    expect(testPantry.itemUsage).to.equal(1);
-    expect(testPantry.itemExpiration).to.equal("02/11/2016");
-    expect(testPantry.itemDate).to.equal("02/01/2016");
+
   });
 });
 
@@ -24,5 +22,5 @@ describe('Shopping', function() {
     var testShopping = new Shopping ("oats", 5);
     expect(testShopping.itemName).to.equal("oats");
     expect(testShopping.itemQuantity).to.equal(5);
-  })
-})
+  });
+});
