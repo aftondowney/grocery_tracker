3dcae80e49bcb2312237e7aa8602912119ccd22a
Haley Bristol and Ramon de la Cruz <hebristol@gmail.com>
2016-02-02 09:32:47 -0800

    added notes into field and resizable textareas

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
-function Pantry(pantryName, pantryQuantity) {
+function Pantry(pantryName, pantryQuantity, notePantry) {
   this.pantryName = pantryName;
   this.pantryQuantity = pantryQuantity;
+  this.notePantry = notePantry;
 }
 
-function Shopping(shopName, shopQuantity) {
+function Shopping(shopName, shopQuantity, noteShopping) {
   this.shopName = shopName;
   this.shopQuantity = shopQuantity;
+  this.noteShopping = noteShopping;
 }
 
 Shopping.prototype.expandList = function(){
-  return this.shopName + "  x" + this.shopQuantity;
+  return this.shopName + "  x" + this.shopQuantity + " (" + this.noteShopping + ")";
 }
 
 Pantry.prototype.expandList = function(){
-  return this.pantryName + "  x" + this.pantryQuantity;
+  return this.pantryName + "  x" + this.pantryQuantity + " (" + this.notePantry + ")";
 }
 
 Pantry.prototype.itemIncrease = function(item){
@@ -26,54 +28,58 @@ Pantry.prototype.itemDecrease = function(item){
     return this.pantryQuantity -= itemsPurchased;
 }
 
+function resetFields() {
+  $("input#shopItem").val("");
+  $("input#shopQuantity").val("");
+  $("input#pantryItem").val("");
+  $("input#pantryQuantity").val("");
+  $("input#notePantry").val("");
+  $("input#noteShopping").val("");
+}
+
 $(document).ready(function() {
   $("#shoppingButton").click(function() {
-    debugger;
     $("#shoppingButton").hide();
 
   });
 
 
   $("form#shoppingForm").submit(function(event) {
-    debugger;
     event.preventDefault();
     var shoppingList = [];
     var shopItem = $("input#shopItem").val();
     var shopQuantity = $("input#shopQuantity").val();
-    var newShopEntry = new Shopping(shopItem, shopQuantity);
+    var noteShopping = $("textarea#noteShopping").val();
+    var newShopEntry = new Shopping(shopItem, shopQuantity, noteShopping);
      shoppingList.push(newShopEntry);
-     $('ul#shoppingList').append("<li><span class='shopping'>" + newShopEntry.expandList() + "</span></li>"); //should this be in the submit function or outside immediately following?
+     $('ul#shoppingList').append("<li><span class='shopping'>" + newShopEntry.expandList() + "</span></li>");
 
+     resetFields();
   });
 
   $("#pantryButton").click(function() {
     $("#pantryButton").hide();
 
-
   });
-
-
 
   $("form#pantryForm").submit(function(event) {
 
+
     var pantryList = [];
     var pantryItem = $("input#pantryItem").val();
-    var pantryQuantity = $("input#pantryQuantity").val();
-    var newPantryEntry = new Pantry(pantryItem, pantryQuantity);
+    var pantryQuantity = $("#pantryQuantity").val();
+    var notePantry = $("textarea#notePantry").val();
+    var newPantryEntry = new Pantry(pantryItem, pantryQuantity, notePantry);
     pantryList.push(newPantryEntry);
-    $('ul#pantryList').append("<li><span class='pantry'>" + newPantryEntry.expandList() + "</span></li>"); //should this be in the submit function or outside immediately following?
+    $('ul#pantryList').append("<li><span class='pantry'>" + newPantryEntry.expandList() + "</span></li>");
+    resetFields();
+
     event.preventDefault();
   });
 
-
-
-    //
-    //
-    //
     // var itemsPurchased = Shopping.shopQuantity;
 
-
-  });
+});
 
 //
 // function resetFields() {
